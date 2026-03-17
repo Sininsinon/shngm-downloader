f72392ef0a7bb33caca9a5c4372c613d55c85bba
sinon <102173440+Sininsinon@users.noreply.github.com>
2026-03-18 00:56:53 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -161,3 +161,5 @@ if st.session_state.manga_data:
                     mime="application/zip",
                     use_container_width=True
                 )
+                 except Exception as e:
+                st.error(f"Terjadi kesalahan: {e}")
